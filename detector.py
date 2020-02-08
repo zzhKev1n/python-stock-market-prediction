61b096256f1a8f7a93341a23319ef2ab4617c29c
Zhihui Zhang <Kev1n@192-168-1-108.tpgi.com.au>
2020-02-08 14:20:22 +1100

    upload

---
+++ detector.py
@@ -0,0 +1,8 @@
+# import libraries
+import math
+import pandas_datareader as web
+import numpy as np 
+import pandas as pd 
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from 
